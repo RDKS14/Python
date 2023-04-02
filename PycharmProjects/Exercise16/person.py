2d7eac440bd615631417efc01b6145f4cd842b24
RDKS14 <rob.smith7@hotmail.co.uk>
2023-04-02 12:07:42 +0100

    python

--- PycharmProjects/Exercise16/person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-
-    def __init__(self, name, gender):
-        self._name = name
-        self._gender = gender.upper()
-
-    def __str__(self):
-        return "Name: " + self._name + " Gender: " + self._gender
